883121f30d7e5eeddf1b22d45e407293e00a25f8
khorshuheng <khor.heng@gojek.com>
2022-06-09 14:29:33 +0800

    Spring initializer project
    Signed-off-by: khorshuheng <khor.heng@gojek.com>

---
+++ src/test/java/dev/caraml/featurestore/CaramlStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.caraml.featurestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaramlStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
