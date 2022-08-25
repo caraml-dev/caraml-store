8283230b596b7ecbf0b01921465a42ff587f29e9
KeshavSharma <keshav.sharma@gojek.com>
2022-08-25 09:30:10 +0800

    e2e test for batch ingestion
    Signed-off-by: KeshavSharma <keshav.sharma@gojek.com>

--- e2e_tests/fixtures/data.py
+++ e2e_tests/fixtures/data.py
@@ -1,17 +1,30 @@
-import os
+import time
 import pytest
 
-from feast import FileSource
-from feast.data_format import ParquetFormat
+from feast import BigQuerySource
+from google.cloud import bigquery
 
-__all__ = "batch_source"
+from _pytest.fixtures import FixtureRequest
+from datetime import datetime
 
+
+__all__ = ("bq_dataset", "batch_source")
+
+@pytest.fixture(scope="session")
+def bq_dataset(pytestconfig):
+    client = bigquery.Client(project=pytestconfig.getoption("bq_project"))
+    timestamp = int(time.time())
+    name = f"feast_e2e_{timestamp}"
+    client.create_dataset(name)
+    yield name
+    client.delete_dataset(name, delete_contents=True)
 
 @pytest.fixture
-def batch_source(local_staging_path: str):
-    return FileSource(
+def batch_source(local_staging_path: str, pytestconfig, request: FixtureRequest):
+    bq_project = "gods-dev"
+    bq_dataset = request.getfixturevalue("bq_dataset")
+    return BigQuerySource(
         event_timestamp_column="event_timestamp",
         created_timestamp_column="created_timestamp",
-        file_format=ParquetFormat(),
-        file_url=os.path.join(local_staging_path, "transactions"),
+        table_ref=f"{bq_project}:{bq_dataset}.source_{datetime.now():%Y%m%d%H%M%s}",
     )
