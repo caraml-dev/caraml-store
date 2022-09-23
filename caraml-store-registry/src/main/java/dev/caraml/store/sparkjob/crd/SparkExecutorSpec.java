f251772db4fb7ac2981047eb0294f56646c2f15e
khorshuheng <khor.heng@gojek.com>
2022-09-23 17:32:00 +0800

    Add support for coreRequest and pod annotations

--- caraml-store-registry/src/main/java/dev/caraml/store/sparkjob/crd/SparkExecutorSpec.java
+++ caraml-store-registry/src/main/java/dev/caraml/store/sparkjob/crd/SparkExecutorSpec.java
@@ -11,9 +11,11 @@ public class SparkExecutorSpec {
   private Map<String, String> nodeSelector;
   private List<V1Toleration> tolerations;
   private Integer cores;
+  private String coreRequest;
   private Integer instances;
   private String memory;
   private String javaOptions;
   private Map<String, String> labels;
+  private Map<String, String> annotations;
   private List<SecretInfo> secrets;
 }
