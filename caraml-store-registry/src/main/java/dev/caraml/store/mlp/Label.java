3485fd4215493544dd86a68227ffebdc4714977e
Shide Foo <shide.foo@gojek.com>
2024-04-02 17:03:55 +0800

    Add changes to handle project labels

---
+++ caraml-store-registry/src/main/java/dev/caraml/store/mlp/Label.java
@@ -0,0 +1,3 @@
+package dev.caraml.store.mlp;
+
+public record Label(String key, String value){}
--- caraml-store-registry/src/main/java/dev/caraml/store/sparkjob/JobTemplateRenderer.java
+++ caraml-store-registry/src/main/java/dev/caraml/store/sparkjob/JobTemplateRenderer.java
@@ -3,22 +3,34 @@ package dev.caraml.store.sparkjob;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.caraml.store.sparkjob.crd.SparkApplicationSpec;
+
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class JobTemplateRenderer {
 
-  public SparkApplicationSpec render(
-      SparkApplicationSpec templateSpec, Map<String, String> context) {
-    ObjectMapper mapper = new ObjectMapper();
-    try {
-      String renderedTemplate = mapper.writeValueAsString(templateSpec);
-      for (String key : context.keySet()) {
-        String value = context.get(key);
-        renderedTemplate = renderedTemplate.replaceAll(String.format("\\$\\{%s}", key), value);
-      }
-      return mapper.readValue(renderedTemplate, SparkApplicationSpec.class);
-    } catch (JsonProcessingException e) {
-      throw new RuntimeException(e);
+    public SparkApplicationSpec render(
+            SparkApplicationSpec templateSpec, Map<String, String> context) {
+        ObjectMapper mapper = new ObjectMapper();
+        HashMap<String, String> unusedLabels = new HashMap<>();
+        try {
+            String oldRenderedTemplate;
+            String renderedTemplate = mapper.writeValueAsString(templateSpec);
+            for (String key : context.keySet()) {
+                String value = context.get(key);
+                oldRenderedTemplate = renderedTemplate;
+                renderedTemplate = renderedTemplate.replaceAll(String.format("\\$\\{%s}", key), value);
+                // nothing got replaced
+                if (renderedTemplate.equals(oldRenderedTemplate)) unusedLabels.put(key, value);
+            }
+            SparkApplicationSpec renderedSpecObject = mapper.readValue(renderedTemplate, SparkApplicationSpec.class);
+            renderedSpecObject.getDriver().getLabels().putAll(unusedLabels);
+            renderedSpecObject.getExecutor().getLabels().putAll(unusedLabels);
+            return renderedSpecObject;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
 }
