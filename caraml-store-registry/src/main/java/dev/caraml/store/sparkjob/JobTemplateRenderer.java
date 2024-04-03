8b7e032e8952f0be5ce165124f49d43a698ddf15
Shide Foo <shide.foo@gojek.com>
2024-04-03 11:37:11 +0800

    Modify job template renderer

--- caraml-store-registry/src/main/java/dev/caraml/store/sparkjob/JobTemplateRenderer.java
+++ caraml-store-registry/src/main/java/dev/caraml/store/sparkjob/JobTemplateRenderer.java
@@ -3,7 +3,7 @@ package dev.caraml.store.sparkjob;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.caraml.store.sparkjob.crd.SparkApplicationSpec;
-import java.util.HashMap;
+
 import java.util.Map;
 
 public class JobTemplateRenderer {
@@ -11,22 +11,15 @@ public class JobTemplateRenderer {
   public SparkApplicationSpec render(
       SparkApplicationSpec templateSpec, Map<String, String> context) {
     ObjectMapper mapper = new ObjectMapper();
-    HashMap<String, String> unusedLabels = new HashMap<>();
     try {
-      String oldRenderedTemplate;
       String renderedTemplate = mapper.writeValueAsString(templateSpec);
       for (String key : context.keySet()) {
         String value = context.get(key);
-        oldRenderedTemplate = renderedTemplate;
         renderedTemplate = renderedTemplate.replaceAll(String.format("\\$\\{%s}", key), value);
-        // nothing got replaced
-        if (renderedTemplate.equals(oldRenderedTemplate)) unusedLabels.put(key, value);
       }
-      SparkApplicationSpec renderedSpecObject =
-          mapper.readValue(renderedTemplate, SparkApplicationSpec.class);
-      renderedSpecObject.getDriver().getLabels().putAll(unusedLabels);
-      renderedSpecObject.getExecutor().getLabels().putAll(unusedLabels);
-      return renderedSpecObject;
+      // replace all unsubstituted label values with empty string
+      renderedTemplate = renderedTemplate.replaceAll("\\$\\{.+\\}$", "");
+      return mapper.readValue(renderedTemplate, SparkApplicationSpec.class);
     } catch (JsonProcessingException e) {
       throw new RuntimeException(e);
     }
