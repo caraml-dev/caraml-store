02a7ec91a9b308ef98e3b511a5b846bb9a307328
Khor Shu Heng <khor.heng@gojek.com>
2024-05-09 17:22:29 +0800

    Enable scheduling for caraml store registry

--- caraml-store-registry/src/main/java/dev/caraml/store/CaraMLRegistry.java
+++ caraml-store-registry/src/main/java/dev/caraml/store/CaraMLRegistry.java
@@ -2,8 +2,10 @@ package dev.caraml.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CaraMLRegistry {
 
   public static void main(String[] args) {
