84cc988a10a91f55c62a38d55e7729dd232dc77a
Vinoth Kumar Sambath <s.vinothkumar@gojek.com>
2025-01-14 12:50:08 +0800

    add support for BATCH_MAXCOMPUTE

--- caraml-store-sdk/python/feast/core/FeatureTable_pb2.py
+++ caraml-store-sdk/python/feast/core/FeatureTable_pb2.py
@@ -18,7 +18,7 @@ from feast.core import Feature_pb2 as feast_dot_core_dot_Feature__pb2
 from feast.core import OnlineStore_pb2 as feast_dot_core_dot_OnlineStore__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1d\x66\x65\x61st/core/FeatureTable.proto\x12\nfeast.core\x1a\x1egoogle/protobuf/duration.proto\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1b\x66\x65\x61st/core/DataSource.proto\x1a\x18\x66\x65\x61st/core/Feature.proto\x1a\x1c\x66\x65\x61st/core/OnlineStore.proto\"f\n\x0c\x46\x65\x61tureTable\x12*\n\x04spec\x18\x01 \x01(\x0b\x32\x1c.feast.core.FeatureTableSpec\x12*\n\x04meta\x18\x02 \x01(\x0b\x32\x1c.feast.core.FeatureTableMeta\"\xb6\x03\n\x10\x46\x65\x61tureTableSpec\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x10\n\x08\x65ntities\x18\x03 \x03(\t\x12)\n\x08\x66\x65\x61tures\x18\x04 \x03(\x0b\x32\x17.feast.core.FeatureSpec\x12\x38\n\x06labels\x18\x05 \x03(\x0b\x32(.feast.core.FeatureTableSpec.LabelsEntry\x12*\n\x07max_age\x18\x06 \x01(\x0b\x32\x19.google.protobuf.Duration\x12,\n\x0c\x62\x61tch_source\x18\x07 \x01(\x0b\x32\x16.feast.core.DataSource\x12-\n\rstream_source\x18\x08 \x01(\x0b\x32\x16.feast.core.DataSource\x12\x36\n\x13staleness_threshold\x18\t \x01(\x0b\x32\x19.google.protobuf.Duration\x12-\n\x0conline_store\x18\n \x01(\x0b\x32\x17.feast.core.OnlineStore\x1a-\n\x0bLabelsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"\xa5\x01\n\x10\x46\x65\x61tureTableMeta\x12\x35\n\x11\x63reated_timestamp\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12:\n\x16last_updated_timestamp\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x10\n\x08revision\x18\x03 \x01(\x03\x12\x0c\n\x04hash\x18\x04 \x01(\tB}\n\x1e\x64\x65v.caraml.store.protobuf.coreB\x11\x46\x65\x61tureTableProtoZHgithub.com/caraml-dev/caraml-store/caraml-store-sdk/go/protos/feast/coreb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1d\x66\x65\x61st/core/FeatureTable.proto\x12\nfeast.core\x1a\x1egoogle/protobuf/duration.proto\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1b\x66\x65\x61st/core/DataSource.proto\x1a\x18\x66\x65\x61st/core/Feature.proto\x1a\x1c\x66\x65\x61st/core/OnlineStore.proto\"r\n\x0c\x46\x65\x61tureTable\x12\x30\n\x04spec\x18\x01 \x01(\x0b\x32\x1c.feast.core.FeatureTableSpecR\x04spec\x12\x30\n\x04meta\x18\x02 \x01(\x0b\x32\x1c.feast.core.FeatureTableMetaR\x04meta\"\xa8\x04\n\x10\x46\x65\x61tureTableSpec\x12\x12\n\x04name\x18\x01 \x01(\tR\x04name\x12\x1a\n\x08\x65ntities\x18\x03 \x03(\tR\x08\x65ntities\x12\x33\n\x08\x66\x65\x61tures\x18\x04 \x03(\x0b\x32\x17.feast.core.FeatureSpecR\x08\x66\x65\x61tures\x12@\n\x06labels\x18\x05 \x03(\x0b\x32(.feast.core.FeatureTableSpec.LabelsEntryR\x06labels\x12\x32\n\x07max_age\x18\x06 \x01(\x0b\x32\x19.google.protobuf.DurationR\x06maxAge\x12\x39\n\x0c\x62\x61tch_source\x18\x07 \x01(\x0b\x32\x16.feast.core.DataSourceR\x0b\x62\x61tchSource\x12;\n\rstream_source\x18\x08 \x01(\x0b\x32\x16.feast.core.DataSourceR\x0cstreamSource\x12J\n\x13staleness_threshold\x18\t \x01(\x0b\x32\x19.google.protobuf.DurationR\x12stalenessThreshold\x12:\n\x0conline_store\x18\n \x01(\x0b\x32\x17.feast.core.OnlineStoreR\x0bonlineStore\x1a\x39\n\x0bLabelsEntry\x12\x10\n\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n\x05value\x18\x02 \x01(\tR\x05value:\x02\x38\x01\"\xdd\x01\n\x10\x46\x65\x61tureTableMeta\x12G\n\x11\x63reated_timestamp\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\x10\x63reatedTimestamp\x12P\n\x16last_updated_timestamp\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.TimestampR\x14lastUpdatedTimestamp\x12\x1a\n\x08revision\x18\x03 \x01(\x03R\x08revision\x12\x12\n\x04hash\x18\x04 \x01(\tR\x04hashB}\n\x1e\x64\x65v.caraml.store.protobuf.coreB\x11\x46\x65\x61tureTableProtoZHgithub.com/caraml-dev/caraml-store/caraml-store-sdk/go/protos/feast/coreb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'feast.core.FeatureTable_pb2', globals())
@@ -29,11 +29,11 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _FEATURETABLESPEC_LABELSENTRY._options = None
   _FEATURETABLESPEC_LABELSENTRY._serialized_options = b'8\001'
   _FEATURETABLE._serialized_start=195
-  _FEATURETABLE._serialized_end=297
-  _FEATURETABLESPEC._serialized_start=300
-  _FEATURETABLESPEC._serialized_end=738
-  _FEATURETABLESPEC_LABELSENTRY._serialized_start=693
-  _FEATURETABLESPEC_LABELSENTRY._serialized_end=738
-  _FEATURETABLEMETA._serialized_start=741
-  _FEATURETABLEMETA._serialized_end=906
+  _FEATURETABLE._serialized_end=309
+  _FEATURETABLESPEC._serialized_start=312
+  _FEATURETABLESPEC._serialized_end=864
+  _FEATURETABLESPEC_LABELSENTRY._serialized_start=807
+  _FEATURETABLESPEC_LABELSENTRY._serialized_end=864
+  _FEATURETABLEMETA._serialized_start=867
+  _FEATURETABLEMETA._serialized_end=1088
 # @@protoc_insertion_point(module_scope)
--- caraml-store-sdk/python/feast/types/Value_pb2.py
+++ caraml-store-sdk/python/feast/types/Value_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17\x66\x65\x61st/types/Value.proto\x12\x0b\x66\x65\x61st.types\"\xe0\x01\n\tValueType\"\xd2\x01\n\x04\x45num\x12\x0b\n\x07INVALID\x10\x00\x12\t\n\x05\x42YTES\x10\x01\x12\n\n\x06STRING\x10\x02\x12\t\n\x05INT32\x10\x03\x12\t\n\x05INT64\x10\x04\x12\n\n\x06\x44OUBLE\x10\x05\x12\t\n\x05\x46LOAT\x10\x06\x12\x08\n\x04\x42OOL\x10\x07\x12\x0e\n\nBYTES_LIST\x10\x0b\x12\x0f\n\x0bSTRING_LIST\x10\x0c\x12\x0e\n\nINT32_LIST\x10\r\x12\x0e\n\nINT64_LIST\x10\x0e\x12\x0f\n\x0b\x44OUBLE_LIST\x10\x0f\x12\x0e\n\nFLOAT_LIST\x10\x10\x12\r\n\tBOOL_LIST\x10\x11\"\x82\x04\n\x05Value\x12\x13\n\tbytes_val\x18\x01 \x01(\x0cH\x00\x12\x14\n\nstring_val\x18\x02 \x01(\tH\x00\x12\x13\n\tint32_val\x18\x03 \x01(\x05H\x00\x12\x13\n\tint64_val\x18\x04 \x01(\x03H\x00\x12\x14\n\ndouble_val\x18\x05 \x01(\x01H\x00\x12\x13\n\tfloat_val\x18\x06 \x01(\x02H\x00\x12\x12\n\x08\x62ool_val\x18\x07 \x01(\x08H\x00\x12\x30\n\x0e\x62ytes_list_val\x18\x0b \x01(\x0b\x32\x16.feast.types.BytesListH\x00\x12\x32\n\x0fstring_list_val\x18\x0c \x01(\x0b\x32\x17.feast.types.StringListH\x00\x12\x30\n\x0eint32_list_val\x18\r \x01(\x0b\x32\x16.feast.types.Int32ListH\x00\x12\x30\n\x0eint64_list_val\x18\x0e \x01(\x0b\x32\x16.feast.types.Int64ListH\x00\x12\x32\n\x0f\x64ouble_list_val\x18\x0f \x01(\x0b\x32\x17.feast.types.DoubleListH\x00\x12\x30\n\x0e\x66loat_list_val\x18\x10 \x01(\x0b\x32\x16.feast.types.FloatListH\x00\x12.\n\rbool_list_val\x18\x11 \x01(\x0b\x32\x15.feast.types.BoolListH\x00\x42\x05\n\x03val\"\x18\n\tBytesList\x12\x0b\n\x03val\x18\x01 \x03(\x0c\"\x19\n\nStringList\x12\x0b\n\x03val\x18\x01 \x03(\t\"\x18\n\tInt32List\x12\x0b\n\x03val\x18\x01 \x03(\x05\"\x18\n\tInt64List\x12\x0b\n\x03val\x18\x01 \x03(\x03\"\x19\n\nDoubleList\x12\x0b\n\x03val\x18\x01 \x03(\x01\"\x18\n\tFloatList\x12\x0b\n\x03val\x18\x01 \x03(\x02\"\x17\n\x08\x42oolList\x12\x0b\n\x03val\x18\x01 \x03(\x08\x42x\n\x1f\x64\x65v.caraml.store.protobuf.typesB\nValueProtoZIgithub.com/caraml-dev/caraml-store/caraml-store-sdk/go/protos/feast/typesb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17\x66\x65\x61st/types/Value.proto\x12\x0b\x66\x65\x61st.types\"\xe0\x01\n\tValueType\"\xd2\x01\n\x04\x45num\x12\x0b\n\x07INVALID\x10\x00\x12\t\n\x05\x42YTES\x10\x01\x12\n\n\x06STRING\x10\x02\x12\t\n\x05INT32\x10\x03\x12\t\n\x05INT64\x10\x04\x12\n\n\x06\x44OUBLE\x10\x05\x12\t\n\x05\x46LOAT\x10\x06\x12\x08\n\x04\x42OOL\x10\x07\x12\x0e\n\nBYTES_LIST\x10\x0b\x12\x0f\n\x0bSTRING_LIST\x10\x0c\x12\x0e\n\nINT32_LIST\x10\r\x12\x0e\n\nINT64_LIST\x10\x0e\x12\x0f\n\x0b\x44OUBLE_LIST\x10\x0f\x12\x0e\n\nFLOAT_LIST\x10\x10\x12\r\n\tBOOL_LIST\x10\x11\"\xac\x05\n\x05Value\x12\x1d\n\tbytes_val\x18\x01 \x01(\x0cH\x00R\x08\x62ytesVal\x12\x1f\n\nstring_val\x18\x02 \x01(\tH\x00R\tstringVal\x12\x1d\n\tint32_val\x18\x03 \x01(\x05H\x00R\x08int32Val\x12\x1d\n\tint64_val\x18\x04 \x01(\x03H\x00R\x08int64Val\x12\x1f\n\ndouble_val\x18\x05 \x01(\x01H\x00R\tdoubleVal\x12\x1d\n\tfloat_val\x18\x06 \x01(\x02H\x00R\x08\x66loatVal\x12\x1b\n\x08\x62ool_val\x18\x07 \x01(\x08H\x00R\x07\x62oolVal\x12>\n\x0e\x62ytes_list_val\x18\x0b \x01(\x0b\x32\x16.feast.types.BytesListH\x00R\x0c\x62ytesListVal\x12\x41\n\x0fstring_list_val\x18\x0c \x01(\x0b\x32\x17.feast.types.StringListH\x00R\rstringListVal\x12>\n\x0eint32_list_val\x18\r \x01(\x0b\x32\x16.feast.types.Int32ListH\x00R\x0cint32ListVal\x12>\n\x0eint64_list_val\x18\x0e \x01(\x0b\x32\x16.feast.types.Int64ListH\x00R\x0cint64ListVal\x12\x41\n\x0f\x64ouble_list_val\x18\x0f \x01(\x0b\x32\x17.feast.types.DoubleListH\x00R\rdoubleListVal\x12>\n\x0e\x66loat_list_val\x18\x10 \x01(\x0b\x32\x16.feast.types.FloatListH\x00R\x0c\x66loatListVal\x12;\n\rbool_list_val\x18\x11 \x01(\x0b\x32\x15.feast.types.BoolListH\x00R\x0b\x62oolListValB\x05\n\x03val\"\x1d\n\tBytesList\x12\x10\n\x03val\x18\x01 \x03(\x0cR\x03val\"\x1e\n\nStringList\x12\x10\n\x03val\x18\x01 \x03(\tR\x03val\"\x1d\n\tInt32List\x12\x10\n\x03val\x18\x01 \x03(\x05R\x03val\"\x1d\n\tInt64List\x12\x10\n\x03val\x18\x01 \x03(\x03R\x03val\"\x1e\n\nDoubleList\x12\x10\n\x03val\x18\x01 \x03(\x01R\x03val\"\x1d\n\tFloatList\x12\x10\n\x03val\x18\x01 \x03(\x02R\x03val\"\x1c\n\x08\x42oolList\x12\x10\n\x03val\x18\x01 \x03(\x08R\x03valBx\n\x1f\x64\x65v.caraml.store.protobuf.typesB\nValueProtoZIgithub.com/caraml-dev/caraml-store/caraml-store-sdk/go/protos/feast/typesb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'feast.types.Value_pb2', globals())
@@ -26,19 +26,19 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _VALUETYPE_ENUM._serialized_start=55
   _VALUETYPE_ENUM._serialized_end=265
   _VALUE._serialized_start=268
-  _VALUE._serialized_end=782
-  _BYTESLIST._serialized_start=784
-  _BYTESLIST._serialized_end=808
-  _STRINGLIST._serialized_start=810
-  _STRINGLIST._serialized_end=835
-  _INT32LIST._serialized_start=837
-  _INT32LIST._serialized_end=861
-  _INT64LIST._serialized_start=863
-  _INT64LIST._serialized_end=887
-  _DOUBLELIST._serialized_start=889
-  _DOUBLELIST._serialized_end=914
-  _FLOATLIST._serialized_start=916
-  _FLOATLIST._serialized_end=940
-  _BOOLLIST._serialized_start=942
-  _BOOLLIST._serialized_end=965
+  _VALUE._serialized_end=952
+  _BYTESLIST._serialized_start=954
+  _BYTESLIST._serialized_end=983
+  _STRINGLIST._serialized_start=985
+  _STRINGLIST._serialized_end=1015
+  _INT32LIST._serialized_start=1017
+  _INT32LIST._serialized_end=1046
+  _INT64LIST._serialized_start=1048
+  _INT64LIST._serialized_end=1077
+  _DOUBLELIST._serialized_start=1079
+  _DOUBLELIST._serialized_end=1109
+  _FLOATLIST._serialized_start=1111
+  _FLOATLIST._serialized_end=1140
+  _BOOLLIST._serialized_start=1142
+  _BOOLLIST._serialized_end=1170
 # @@protoc_insertion_point(module_scope)
