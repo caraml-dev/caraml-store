78b4dc161cd94177f33b318f2a7d1836534ad2e1
Vinoth Kumar Sambath <s.vinothkumar@gojek.com>
2025-01-31 21:40:46 +0800

    fix format

--- caraml-store-spark/src/main/scala/dev/caraml/spark/sources/maxCompute/MaxComputeReader.scala
+++ caraml-store-spark/src/main/scala/dev/caraml/spark/sources/maxCompute/MaxComputeReader.scala
@@ -11,14 +11,15 @@ import com.caraml.odps.CustomDialect
 
 object MaxComputeReader {
   def createBatchSource(
-       sparkSession: SparkSession,
-       source: MaxComputeSource,
-       start: DateTime,
-       end: DateTime
+        sparkSession: SparkSession,
+        source: MaxComputeSource,
+        start: DateTime,
+        end: DateTime
   ): DataFrame = {
-    val maxComputeAccessID = sys.env("CARAML_SPARK_MAXCOMPUTE_ACCESS_ID")
+    val maxComputeAccessID  = sys.env("CARAML_SPARK_MAXCOMPUTE_ACCESS_ID")
     val maxComputeAccessKey = sys.env("CARAML_SPARK_MAXCOMPUTE_ACCESS_KEY")
-    val maxComputeJDBCConnectionURL = "jdbc:odps:https://service.ap-southeast-5.maxcompute.aliyun.com/api/?project=%s" format source.project
+    val maxComputeJDBCConnectionURL =
+      "jdbc:odps:https://service.ap-southeast-5.maxcompute.aliyun.com/api/?project=%s" format source.project
 
     val sqlQuery = "select * from %s.%s  where to_millis(%s) > %d and to_millis(%s) < %d" format (
       source.dataset, source.table, source.eventTimestampColumn, start.getMillis, source.eventTimestampColumn, end.getMillis
@@ -27,13 +28,14 @@ object MaxComputeReader {
     val customDialect = new CustomDialect()
     JdbcDialects.registerDialect(customDialect)
 
-    val data = sparkSession.read.format("jdbc")
+    val data = sparkSession.read
+      .format("jdbc")
       .option("url", maxComputeJDBCConnectionURL)
       // Not setting queryTimeout will fail the query, whereas setting it up actually doesn't make an impact
       .option("queryTimeout", 5000)
       .option("query", sqlQuery)
-      .option("user", maxComputeAccessID) // access id
-      .option("password", maxComputeAccessKey) //
+      .option("user", maxComputeAccessID)
+      .option("password", maxComputeAccessKey)
       .load()
 
     data.toDF()
