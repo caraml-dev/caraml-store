3143b99a369750449e7a8b137e743337462a6f5e
Vinoth Kumar Sambath <s.vinothkumar@gojek.com>
2025-02-10 12:27:39 +0800

    refactor|

--- caraml-store-spark/src/main/scala/dev/caraml/spark/sources/maxCompute/CustomDialect.scala
+++ caraml-store-spark/src/main/scala/dev/caraml/spark/sources/maxCompute/CustomDialect.scala
@@ -4,18 +4,14 @@ import org.apache.spark.sql.jdbc.JdbcDialect
 
 class CustomDialect extends JdbcDialect {
     override def canHandle(url: String): Boolean = {
-        println("can handle? this one")
-        println(url.startsWith("jdbc:odps"), url)
         url.startsWith("jdbc:odps")
     }
 
     override def quoteIdentifier(colName: String): String = {
-        println("inside quote identifier", colName, s"$colName")
         s"$colName"
     }
 
     override def getSchemaQuery(table: String): String = {
-        println("getschemaquery", table)
         table
     }
 }
--- caraml-store-spark/src/main/scala/dev/caraml/spark/sources/maxCompute/MaxComputeReader.scala
+++ caraml-store-spark/src/main/scala/dev/caraml/spark/sources/maxCompute/MaxComputeReader.scala
@@ -21,17 +21,10 @@ object MaxComputeReader {
     val sqlQuery = "(select * from `%s.%s`  where to_millis(%s) > %d and to_millis(%s) < %d)" format (
       source.dataset, source.table, source.eventTimestampColumn, start.getMillis, source.eventTimestampColumn, end.getMillis
     )
-
-//    val sqlQuery = "(select * from `%s.%s`)" format (
-//      source.dataset, source.table
-//    )
     println("query is", sqlQuery)
 
-    println(JdbcDialects.get("jdbc:odps:https://service.ap-southeast-5.maxcompute.aliyun.com/api/?project=%s&interactiveMode=True&enableLimit=False" format source.project))
     val customDialect = new CustomDialect()
     JdbcDialects.registerDialect(customDialect)
-    println("custom dialect registered")
-    println(JdbcDialects.get("jdbc:odps:https://service.ap-southeast-5.maxcompute.aliyun.com/api/?project=%s&interactiveMode=True&enableLimit=False" format source.project))
 
     val data = sparkSession.read
       .format("jdbc")
@@ -39,19 +32,12 @@ object MaxComputeReader {
       // Not setting queryTimeout will fail the query, whereas setting it up actually doesn't make an impact
       .option("queryTimeout", 5000)
       .option("dbtable", sqlQuery)
-// ,option("query", sqlQuery)
       .option("user", maxComputeAccessID)
       .option("password", maxComputeAccessKey)
       .load()
 
-    println(data)
-    println(data.toDF().show(3))
-    println(data.toDF().count())
+    println("total rows fetched from maxcompute", data.toDF().count())
 
-//    data.toDF().registerTempTable("temp_table")
-//    val valres = sparkSession.sql("select * from temp_table")
-//
-//    println("result from query", valres)
     data.toDF()
   }
 }
